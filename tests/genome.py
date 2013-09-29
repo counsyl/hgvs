c12252a100632c784a063792e2e39f2384022698
Matt Rasmussen <rasmus@counsyl.com>
2013-09-28 17:37:05 -0700

    add variant tests

---
+++ tests/genome.py
@@ -0,0 +1,55 @@
+try:
+    from pygr.seqdb import SequenceFileDB
+except:
+    pass
+
+
+_genome_file = '/seq-data/seq/misc/genomics/genomes/hg19/hg19.fa'
+
+
+class MockSequence(object):
+    def __init__(self, sequence):
+        self.sequence = sequence
+
+    def __neg__(self):
+        return MockSequence(revcomp(self.sequence))
+
+    def __str__(self):
+        return self.sequence
+
+
+class MockChromosome(object):
+    def __init__(self, name, genome=None):
+        self.name = name
+        self.genome = genome
+
+    def __getslice__(self, start, end, step=1):
+        return self.genome.get_seq(self.name, start, end)
+
+
+class MockGenome(object):
+    def __init__(self, lookup=None):
+        self._chroms = {}
+        self._lookup = lookup
+
+    def __getitem__(self, chrom):
+        if chrom in self._chroms:
+            return self._chroms[chrom]
+        else:
+            chromosome = MockChromosome(chrom, self)
+            self._chroms[chrom] = chromosome
+            return chromosome
+
+    def get_seq(self, chrom, start, end):
+        return self._lookup[(chrom, start, end)]
+
+
+class MockGenomeFile(MockGenome):
+    def __init__(self, filename=_genome_file):
+        super(MockGenomeFile, self).__init__()
+        self._genome = SequenceFileDB(filename)
+
+    def get_seq(self, chrom, start, end):
+        seq = self._genome[chrom][start:end]
+        print ((chrom, start, end), str(seq))
+        return seq
---
+++ tests/test_variants.py
@@ -0,0 +1,60 @@
+
+from collections import defaultdict
+from unittest import TestCase
+
+from ..variants import normalize_variant
+from ..variants import revcomp
+from .genome import MockGenome
+
+
+_genome_seq = dict([
+    (('chr17', 41246219, 41246249), 'TTACATATTAAAGCCTCATGAGGATCACTG'),
+    (('chr17', 41246220, 41246250), 'TACATATTAAAGCCTCATGAGGATCACTGG'),
+    (('chr17', 41246219, 41246249), 'TTACATATTAAAGCCTCATGAGGATCACTG'),
+    (('chr17', 41246217, 41246247), 'TTTTACATATTAAAGCCTCATGAGGATCAC'),
+    (('chr17', 41246217, 41246247), 'TTTTACATATTAAAGCCTCATGAGGATCAC'),
+    (('chr17', 41246217, 41246247), 'TTTTACATATTAAAGCCTCATGAGGATCAC'),
+])
+
+
+_normalize_tests = [
+    # Simple SNP.
+    (('chr17', 41246250, 'G', ['C']),
+     ('chr17', 41246250, 'G', ['C'])),
+
+    # Left-align and 1bp pad.
+    (('chr17', 41246251, '', ['G']),
+     ('chr17', 41246248, 'T', ['TG'])),
+
+    # Trim common prefix, left-align, and 1bp pad.
+    (('chr17', 41246250, 'G', ['GG']),
+     ('chr17', 41246248, 'T', ['TG'])),
+
+    # Trim common prefix.
+    (('chr17', 41246248, 'TGGC', ['TGGA']),
+     ('chr17', 41246251, 'C', ['A'])),
+
+    # Trim common prefix and suffix.
+    (('chr17', 41246248, 'TGGC', ['TGAC']),
+     ('chr17', 41246250, 'G', ['A'])),
+
+    # Trim common prefix, triallelic
+    (('chr17', 41246248, 'TGGC', ['TGGA', 'TGAC']),
+     ('chr17', 41246249, 'GGC', ['GAC', 'GGA'])),
+]
+
+
+class TestVariant(TestCase):
+    def test_normalize_variant(self):
+        """
+        Test normalize_variant against known cases.
+        """
+        genome = MockGenome(_genome_seq)
+
+        for variant, true_variant in _normalize_tests:
+            chrom, offset, ref, alts = variant
+            norm_variant = normalize_variant(
+                chrom, offset, ref, alts, genome).variant
+            self.assertEqual(norm_variant, true_variant,
+                             'Variant failed to normalize: %s' %
+                             repr(variant))
