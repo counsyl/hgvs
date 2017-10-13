4b312d23b58a631b042789500b101a1b371c94ad
Christoph Klein <cklein@counsyl.com>
2017-10-13 12:37:32 -0700

    Cleanup setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-
 from __future__ import print_function
 from __future__ import unicode_literals
+
 from setuptools import setup
 from pip.req import parse_requirements
 from pip.download import PipSession
@@ -11,6 +11,7 @@ description = ("This library provides a simple to use Python API for parsing, "
                "formatting, and normalizing variant names specified in the "
                "standard recommended by the Human Genome Variation Society "
                "(HGVS).")
+
 
 def main():
     python_version = sys.version_info
@@ -28,15 +29,13 @@ def main():
         author_email='rasmus@counsyl.com',
         packages=['pyhgvs', 'pyhgvs.tests'],
         include_package_data=True,
-        package_data={
-            '': ['requirements-dev.txt'],
-        },
         scripts=[],
         install_requires=['pip>=1.2'],
         tests_require=[str(line.req) for line in
                        parse_requirements('requirements-dev.txt',
                                           session=PipSession())],
     )
+
 
 if __name__ == '__main__':
     main()
