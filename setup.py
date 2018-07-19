5c3ad2f71492dfa0bff1bb85c4fcd4a8daf4df0a
Alcides Fonseca <me@alcidesfonseca.com>
2018-07-19 14:18:05 +0100

    Added support for pip 10 install

--- setup.py
+++ setup.py
@@ -1,41 +1,47 @@
 #!/usr/bin/env python
 
-from setuptools import setup
-from pip.req import parse_requirements
-from pip.download import PipSession
-import sys
+import os.path
 
-description = ("This library provides a simple to use Python API for parsing, "
-               "formatting, and normalizing variant names specified in the "
-               "standard recommended by the Human Genome Variation Society "
-               "(HGVS).")
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 
-def main():
-    python_version = sys.version_info
-    if python_version < (2, 6):
-        print ("This library requires Python version >=2.6, "
-               "You have version %d.%d" % python_version[:2])
-        sys.exit(1)
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
 
-    setup(
-        name='pyhgvs',
-        version='0.9.4',
-        description='HGVS name parsing and formatting',
-        long_description=description,
-        author='Matt Rasmussen',
-        author_email='rasmus@counsyl.com',
-        packages=['pyhgvs', 'pyhgvs.tests'],
-        include_package_data=True,
-        package_data={
-            '': ['requirements-dev.txt'],
-        },
-        scripts=[],
-        install_requires=['pip>=1.2'],
-        tests_require=[str(line.req) for line in
-                       parse_requirements('requirements-dev.txt',
-                                          session=PipSession())],
+requires = [
+        'beautifulsoup4==4.4.1'
+    ]
+    
+test_requires = [
+    'nose==1.3.4'
+    'flake8==2.2.5'
+    'pygr==0.8.2'
+]
+
+setup(
+    name='pyhgvs',
+    version='0.9.4',
+    author='Matt Rasmussen',
+    author_email='rasmus@counsyl.com',
+    url='https://github.com/counsyl/hgvs/',
+    description='HGVS name parsing and formatting',
+    long_description=read('README.md'),
+    py_modules=('pyhgvs', 'pyhgvs.tests'),
+    python_requires=">=2.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    install_requires=requires,
+    tests_require=test_requires,
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
     )
-
-if __name__ == '__main__':
-    main()
+)
