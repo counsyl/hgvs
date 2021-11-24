a0a8edddfbf36cfa100a650719105b7e4c65fd0d
Dave Lawrence <davmlaw@gmail.com>
2021-11-24 17:26:18 +1030

    Bump version so I can track it

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def main():
 
     setup(
         name='pyhgvs',
-        version='0.11.1',
+        version='0.12.0',
         description='HGVS name parsing and formatting',
         long_description=description,
         author='Matt Rasmussen',
